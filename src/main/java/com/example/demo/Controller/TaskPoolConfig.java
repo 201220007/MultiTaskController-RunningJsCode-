3332f96c10e2c5da2a7d9843678e4c13bab3fdd8
Shi Xi <rdpwl.shixi.ext@siemens.com>
2022-08-30 09:54:00 +0800

    djj

---
+++ src/main/java/com/example/demo/Controller/TaskPoolConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.demo.bean.AsyncConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+@Configuration
+@EnableAsync
+@ComponentScan("com.example.demo")
+public class TaskPoolConfig {
+    @Bean(name = "primaryExecutor")
+    public ThreadPoolTaskExecutor primaryExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 设置核心线程数,它是可以同时被执行的线程数量
+        executor.setCorePoolSize(2);
+        // 设置最大线程数,缓冲队列满了之后会申请超过核心线程数的线程
+        executor.setMaxPoolSize(10);
+        // 设置缓冲队列容量,在执行任务之前用于保存任务
+        executor.setQueueCapacity(50);
+        // 设置线程生存时间（秒）,当超过了核心线程出之外的线程在生存时间到达之后会被销毁
+        executor.setKeepAliveSeconds(60);
+        // 设置线程名称前缀
+        executor.setThreadNamePrefix("imagePool-");
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        //初始化
+        executor.initialize();
+        return executor;
+    }
+
+}
+
